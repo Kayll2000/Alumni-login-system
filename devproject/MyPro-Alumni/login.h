3edba2e376a8cddcb7abd1c6ad13212df9677a65
chenjunlong <2825363541@qq.com>
2023-04-27 15:04:02 +0800

    [feature]加入校友信息读取API，兼容原有程序

--- devproject/MyPro-Alumni/login.h
+++ devproject/MyPro-Alumni/login.h
@@ -7,7 +7,23 @@
 #include <vector>
 using namespace std;
 #define ALUMNIFILE "./Debug/AlumniData/AlumniFILE.txt"    //保存为TXT文本格式
+#define ALUMNISAVETOREAD "./Debug/AlumniData/AlumniSaveToreadFILE.txt"    //保存以便读取的文件
+#define AALLDATA "./Debug/ALLNUMData/Aalldata.txt"//保存校友总个数
+//static int alumninum;//校友总数
+extern int alumninum;//校友总数 extern全局，因为main需要使用。
+static int alumninumread = 0;//记录读取校友总条数  static 
+extern bool InitFlag;
 //"login.h"
+//存放读取的数据
+struct ADATA {//read 用来读取校友数据
+    string _name;        //姓名
+    string _student_id;  //学号
+    string _password;    //密码
+    string _gender;      //性别
+    string _birthdate;   //出生日期
+    string _phone;       //联系电话
+    string _email;       //电子邮件
+};
 //校友信息
 class Alumni {
 private:
@@ -46,6 +62,10 @@ public:
     void modifyAlumni(Alumni alumni);
     void deleteAlumni(string student_id);
     void savealumniinfo();
+    void SaveAlumni_ToRead();
+    void Save_Nalldata();
+    void Read_AlummiNumData();
+    void Init_AlumniDate();
 };
 
 class Admin {
--- devproject/MyPro-Alumni/main.cpp
+++ devproject/MyPro-Alumni/main.cpp
@@ -23,7 +23,7 @@
 	                        2.5 新闻功能
 	                        2.6 问卷功能
 	                        2.7 退出登录
-            BUGFIX:
+            BUGFIX:[2023.04.27]解决显示校友总数一直为0的bug。
             MODIFY:[2023.04.24]增加主函数登录界面输入的容错性，优化主界面，优化UI。
                    [2023.04.27]优化登录界面。
                    [2023.04.27]优化校友查看新闻信息功能。
@@ -99,6 +99,8 @@ int main()
                     }
                     else if (choice == 2) {
                         vector<Alumni> alumni_list = alumni_manager.getAlumniList();
+                        cout << "<<校友总数>> " << alumninum << endl;
+                        
                         for (int i = 0; i < alumni_list.size(); i++) {
                             if (alumni_list[i].getStudentId() != student_id) {
                                 cout << "姓名：" << alumni_list[i].getName() << endl;
@@ -147,17 +149,19 @@ int main()
             if (admin_manager.login(username, password)) {
                 system("cls");
                 while (true) {
-                    cout << "请选择操作：" << endl;
-                    cout << "[1] 查看校友列表" << endl;
-                    cout << "[2] 添加校友信息" << endl;
-                    cout << "[3] 修改校友信息" << endl;
-                    cout << "[4] 删除校友信息" << endl;
-                    cout << "[5] 新闻功能" << endl;
-                    cout << "[6] 问卷功能" << endl;
-                    cout << "[7] 退出登录" << endl;
+                    cout << "\t\t\t\t请选择操作>>>" << endl;
+                    cout << "\t\t\t\t[1] 查看校友列表" << endl;
+                    cout << "\t\t\t\t[2] 添加校友信息" << endl;
+                    cout << "\t\t\t\t[3] 修改校友信息" << endl;
+                    cout << "\t\t\t\t[4] 删除校友信息" << endl;
+                    cout << "\t\t\t\t[5] 新闻功能" << endl;
+                    cout << "\t\t\t\t[6] 问卷功能" << endl;
+                    cout << "\t\t\t\t[7] 初始化系统校友数据" << endl;
+                    cout << "\t\t\t\t[8] 退出登录" << endl;
                     cin >> choice;
                     if (choice == 1) {
                         vector<Alumni> alumni_list = alumni_manager.getAlumniList();
+                        cout << "<<校友总数>> " << alumninum << endl;
                         for (int i = 0; i < alumni_list.size(); i++) {
                             cout << "姓名：" << alumni_list[i].getName() << endl;
                             cout << "学号：" << alumni_list[i].getStudentId() << endl;
@@ -176,6 +180,7 @@ int main()
                         cin >> name >> student_id >> password >> gender >> birthdate >> phone >> email;
                         Alumni alumni(name, student_id, password, gender, birthdate, phone, email);
                         alumni_manager.addAlumni(alumni);
+                        // alumninum++;
                         cout << "添加成功！" << endl;
                         system("pause");
                         system("cls");
@@ -208,7 +213,7 @@ int main()
                         string student_id;
                         cin >> student_id;
                         alumni_manager.deleteAlumni(student_id);
-                        cout << "删除成功！" << endl;
+                        //alumninum--;
                         system("pause");
                         system("cls");
                     }
@@ -226,11 +231,23 @@ int main()
                     	system("cls");
                         admin_manager.questionfun();//问卷功能
                     }
-                    else if (choice == 7) {
+                    else if(choice == 7){
+                        system("cls");
+                        if(InitFlag)
+                        {
+                            alumni_manager.Init_AlumniDate();//init
+                            InitFlag = false;//只初始化一次
+                        }else{
+                            cout << "数据已初始化！" << endl;
+                            system("pause");
+                            system("cls");
+                        }
+                    }
+                    else if (choice == 8) {
                         system("pause");
                         system("cls");
                         break;
-                    }                    
+                    }        
                     else {
                         cout << "输入有误，请重新输入！" << endl;
                         system("pause");
